09def2598ecb047b1197f7aa9d953b1d1e754c7f
Mrick343 <mrinmoy8429de@gmail.com>
2025-12-24 21:27:11 +0000

    updtae pif

--- gms_certified_props.mk
+++ gms_certified_props.mk
@@ -2,14 +2,15 @@
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.pihooks_MANUFACTURER?=Google \
     persist.sys.pihooks_BRAND?=google \
-    persist.sys.pihooks_PRODUCT?=mustang_beta \
-    persist.sys.pihooks_DEVICE?=mustang \
-    persist.sys.pihooks_ID?=BP41.250916.010.A1 \
+    persist.sys.pihooks_PRODUCT?=caiman_beta \
+    persist.sys.pihooks_DEVICE?=caiman \
+    persist.sys.pihooks_ID?=CP11.251114.006 \
     persist.sys.pihooks_RELEASE?=16 \
-    persist.sys.pihooks_SECURITY_PATCH?=2025-10-05 \
-    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=32 \
-    persist.sys.pihooks_SDK_INT?=36
+    persist.sys.pihooks_SECURITY_PATCH?=2025-12-05 \
+    persist.sys.pihooks_DEVICE_INITIAL_SDK_INT?=21 \
+    persist.sys.pihooks_SDK_INT?=32
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PihooksGmsFp="google/mustang_beta/mustang:16/BP41.250916.010.A1/14281945:user/release-keys" \
-    PihooksGmsModel="Pixel 10 Pro XL"
+    BuildFingerprint=google/caiman_beta/caiman:16/CP11.251114.006/14560987:user/release-keys \
+    PihooksGmsFp="google/caiman_beta/caiman:16/CP11.251114.006/14560987:user/release-keys" \
+    PihooksGmsModel="Pixel 9"
